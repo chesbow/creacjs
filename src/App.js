dff628f7f6fea259f92182313fe9f5c55b32970d
Chi Bao <1510169@hcmute.edu.vn>
2021-07-21 18:23:32 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import axios from 'axios';
+
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/company')
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.persons.map(doituong => <div>{doituong.nguoi1}</div>)}
+      </ul>
+    )
+  }
+}
+
+/*export default class PersonList extends React.Component {
+  state = {
+    name: '',
+  }
+
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      name: this.state.name
+    };
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person Name:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    )
+  }
+}*/
